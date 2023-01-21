a8eab2b69de8fa5666eae0fb063c25723fb890a7
Miljan Pantic <miljan.pantic@radancy.com>
2023-01-22 00:08:26 +0100

    API: Create FastEndpoints API

---
+++ src/ApiPerformanceTest.FastEndpointsApi/Endpoints/GetWeatherForecastEndpoint.cs
@@ -0,0 +1,31 @@
+﻿using FastEndpoints;
+
+namespace ApiPerformanceTest.FastEndpointsApi.Endpoints;
+
+internal class GetWeatherForecastEndpoint : EndpointWithoutRequest
+{
+    private static readonly string[] Summaries = 
+    {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+    
+    public override void Configure()
+    {
+        Verbs(Http.GET);
+        Routes("WeatherForecast");
+        AllowAnonymous();
+    }
+
+    public override async Task HandleAsync(CancellationToken ct)
+    {
+        var forecast = Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+
+        await SendAsync(forecast, cancellation: ct);
+    }
+}
