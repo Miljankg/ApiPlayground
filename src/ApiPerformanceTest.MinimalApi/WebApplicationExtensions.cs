30debc897ccfaa4df71e8da6f53626271394eb15
Miljan Pantic <miljan.pantic@radancy.com>
2023-01-21 23:57:28 +0100

    API: Create Minimal API

--- src/ApiPerformanceTest.MinimalApi/WebApplicationExtensions.cs
+++ src/ApiPerformanceTest.MinimalApi/WebApplicationExtensions.cs
@@ -1,6 +1,25 @@
 ﻿namespace ApiPerformanceTest.MinimalApi;
 
-public class WebApplicationExtensions
+internal static class WebApplicationExtensions
 {
+    private static readonly string[] Summaries =
+    {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
     
+    public static IEndpointConventionBuilder MapWeatherForecastEndpoints(this WebApplication app)
+    {
+        return app.MapGet("/WeatherForecast", () =>
+        {
+            var forecast = Enumerable.Range(1, 5).Select(index => new WeatherForecast
+                {
+                    Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                    TemperatureC = Random.Shared.Next(-20, 55),
+                    Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+                })
+                .ToArray();
+
+            return Task.FromResult(forecast);
+        });
+    }
 }
